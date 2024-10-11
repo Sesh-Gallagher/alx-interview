56b5bf323f2d1422991cf22df96e87c4524aec31
root <t.ntwana@gmail.com>
2024-10-10 20:11:32 -0700

    Basic annotations - floor

---
+++ alx-backend-python/0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+Module that represents type-annotated function floor
+which takes a float n as argument and returns
+the float truncated to floor
+"""
+
+
+def floor(n: float) -> int:
+    """
+    args: n: float
+
+    Return: float truncated to floor
+    """
+
+    return int(n)
