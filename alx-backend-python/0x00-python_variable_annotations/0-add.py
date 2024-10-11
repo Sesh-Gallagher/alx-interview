6a23f33ceb867e060c862c0b48ed96e7865e7737
root <t.ntwana@gmail.com>
2024-10-10 19:38:55 -0700

    Basic annotations - add

---
+++ alx-backend-python/0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+Module represents type-annotated function add
+that takes a float a and a float b as
+arguments and returns their sum as a float.
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+  Args: a: The first parameter to be added
+        b: The second parameter
+
+  Return: sum of two floats
+    """
+
+    return (a + b)
---
+++ alx-backend-python/0x00-python_variable_annotations/README.md
@@ -0,0 +1,1 @@
+This readme contauns information regarding this file and its contents
