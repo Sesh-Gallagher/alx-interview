5e1509cdfae4a8df151ca7bb3942bb6c9eed0389
root <t.ntwana@gmail.com>
2024-10-10 19:51:49 -0700

    Basic annotations - concat

---
+++ alx-backend-python/0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,15 @@
+#!usr/bin/env python3
+"""
+Module that represents type-annotated function concat
+that takes a string str1 and a string str2 as arguments
+and returns a concatenated string
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+
+   args: str1: first parameter
+         str2: second parameter
+    """
+    return str1 + str2
